73f73d0af2ac08973d25fedabb450932818ff1e5
Pyroarsonist <velikiydan@gmail.com>
2025-09-04 12:21:39 +0300

    added wasm files to build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -8,4 +8,14 @@ export default defineConfig({
   sourcemap: true,
   failOnWarn: true,
   noExternal: () => true,
+  copy: [
+    {
+      from: "node_modules/@prisma/prisma-schema-wasm/src/prisma_schema_build_bg.wasm",
+      to: "dist/prisma_schema_build_bg.wasm",
+    },
+    {
+      from: "node_modules/@prisma/schema-engine-wasm/schema_engine_bg.wasm",
+      to: "dist/schema_engine_bg.wasm",
+    },
+  ],
 });
